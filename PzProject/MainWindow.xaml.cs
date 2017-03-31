9217dc9a60f6e97da8ad40c0aeef42f7297307ec
kpiega <kamil.piega@gmail.com>
2017-03-31 19:10:36 +0200

    template project

--- PzProject/MainWindow.xaml.cs
+++ PzProject/MainWindow.xaml.cs
@@ -1,29 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MahApps.Metro.Controls;
+using PzProject.Utility;
+using PzProject.View;
 
 namespace PzProject
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
-            // test
             InitializeComponent();
+            NavigationManager.Initialize(MainFrame);
+            NavigationManager.NavigateTo(new MainPage());
         }
     }
 }
