bb171feb961c72eec6ddb22fe645a18b6f38685a
Dawid <Dawid@DAWID-PC>
2017-04-02 13:40:06 +0200

    zmiana 123

--- PzProject/View/MainPage.xaml.cs
+++ PzProject/View/MainPage.xaml.cs
@@ -25,6 +25,7 @@ namespace PzProject.View
             InitializeComponent();
             _viewModel = new MainPageViewModel();
             this.DataContext = _viewModel;
+            // zmiany 123
         }
     }
 }
