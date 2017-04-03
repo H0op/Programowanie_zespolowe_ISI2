49609ca4e329cdad6d53c4094439e6bfb3d9b701
kpiega <kamil.piega@gmail.com>
2017-04-03 20:02:54 +0200

    room view

---
+++ PzProject/ViewCreator/RoomViewCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using PzProject.Model;
+
+namespace PzProject.ViewCreator
+{
+    public class RoomViewCreator
+    {
+        private ObservableCollection<Spot> _spots;
+        private Room _room;
+
+
+    }
+}
