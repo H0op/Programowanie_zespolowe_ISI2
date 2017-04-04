17d6047f97aede13e42057b9b2170d53baf04520
Patryk Tartakowski <p.tartakowski@hotmail.com>
2017-04-04 13:22:33 +0200

    Sala gotowa

---
+++ PzProject/Model/SpotButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using PzProject.Model;
+
+namespace PzProject.ViewModel
+{
+    class SpotButton : Button
+    {
+        private Spot _spot;
+
+        public SpotButton(Spot spot)
+        {
+            this._spot = spot;
+            this.MaxWidth = 50;
+            this.MaxHeight = 50;
+            this.Content = spot.Column + "\n" + spot.Row;
+            this.Background = !spot.IsAvailable ? Brushes.Gray : Brushes.Red;
+        }
+
+        public Spot Spot
+        {
+            get { return _spot; }
+            private set { _spot = value; }
+        }
+    }
+}
